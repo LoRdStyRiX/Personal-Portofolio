64f74c8fb0f306886493668d0d889587fd769f99
LoRdStyRiX <raihan.aframadhan@gmail.com>
2024-12-07 10:42:21 +0700

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sharpe-personal': ['Sharpe Personal', 'sans-serif'],
+        'helvetica-neue-55': ['Helvetica Neue 55', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
